409714de66e7e25b2c8f8635b3435d7a7522c714
Billy Dai <2640736909@qq.com>
2015-02-26 09:52:48 +0800

    init

---
+++ src/main/java/wenxin/billyspring/App.java
@@ -0,0 +1,13 @@
+package wenxin.billyspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
