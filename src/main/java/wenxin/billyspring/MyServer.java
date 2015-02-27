dcd6dbcf5e117378ff1a089df6fafa8ef15ce935
Billy Dai <2640736909@qq.com>
2015-02-27 17:27:29 +0800

    resource handler from xml

--- src/main/java/wenxin/billyspring/MyServer.java
+++ src/main/java/wenxin/billyspring/MyServer.java
@@ -17,36 +17,38 @@ public class MyServer {
 		{
 			ApplicationContext context = new ClassPathXmlApplicationContext(
 					new String[] { "/wenxin/billyspring/spring.xml" });
+			// //
 			// System.out.print(Arrays.toString(context.getBeanDefinitionNames()));
-			// stop and restart server for debug purpose
-			Server server = (Server) context.getBean("Server");
-			// WebAppContext staticcontext = new WebAppContext();
-			// staticcontext.setContextPath("/ext");
-			// String ProPath = System.getProperty("user.dir");
-			// staticcontext.setDescriptor(ProPath
-			// + "/src/main/webapp/WEB-INF/web.xml");
-			// staticcontext.setResourceBase(ProPath + "/src/main/webapp");
-			// staticcontext.setParentLoaderPriority(true);
-			Handler[] handlers = server.getHandlers();
-			ResourceHandler resource_handler = new ResourceHandler();
-			resource_handler.setDirectoriesListed(false);
-			resource_handler.setWelcomeFiles(new String[] { "index.html" });
-
-			resource_handler.setResourceBase("webroot");
-			server.stop();
-			// server.setHandler(staticcontext);
-			Vector<Handler> c = new Vector<Handler>(handlers.length + 1);
-			c.add(resource_handler);
-			for (Handler handler : handlers) {
-				c.add(handler);
-			}
-			Handler[] morehandlers = (Handler[]) c.toArray(new Handler[0]);
-
-			HandlerList handlerlists = new HandlerList();
-			handlerlists.setHandlers(morehandlers);
-			server.setHandler(handlerlists);
+			// // stop and restart server for debug purpose
+			// Server server = (Server) context.getBean("Server");
+			// // WebAppContext staticcontext = new WebAppContext();
+			// // staticcontext.setContextPath("/ext");
+			// // String ProPath = System.getProperty("user.dir");
+			// // staticcontext.setDescriptor(ProPath
+			// // + "/src/main/webapp/WEB-INF/web.xml");
+			// // staticcontext.setResourceBase(ProPath + "/src/main/webapp");
+			// // staticcontext.setParentLoaderPriority(true);
+			// Handler[] handlers = server.getHandlers();
+			// ResourceHandler resource_handler = new ResourceHandler();
+			// resource_handler.setDirectoriesListed(false);
+			// resource_handler.setWelcomeFiles(new String[] { "index.html" });
+			//
+			// resource_handler.setResourceBase("webroot");
+			// server.stop();
+			// // server.setHandler(staticcontext);
+			// Vector<Handler> c = new Vector<Handler>(handlers.length + 1);
+			// c.add(resource_handler);
+			// for (Handler handler : handlers) {
+			// c.add(handler);
+			// }
+			// Handler[] morehandlers = (Handler[]) c.toArray(new Handler[0]);
+			//
+			// HandlerList handlerlists = new HandlerList();
+			// handlerlists.setHandlers(morehandlers);
+			// server.setHandler(handlerlists);
+			// //
 			// System.out.print(Arrays.toString(context.getBeanDefinitionNames()));
-			server.start();
+			// server.start();
 		}
 	}
 }
---
+++ webroot/hideindex.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Static content home</title>
+</head>
+<body>
+Welcome.
+<%=newjava.util.Date()%>
+</body>
+</html>
