4fbb003c97887f044633ab2f99a264b009a0907e
Billy Dai <2640736909@qq.com>
2015-03-03 16:45:11 +0800

    remove unused methods from aspect AopHandler.

--- src/main/java/wenxin/billyspring/AopHandler.java
+++ src/main/java/wenxin/billyspring/AopHandler.java
@@ -1,25 +1,11 @@
 package wenxin.billyspring;
 
 public class AopHandler {
-
-	/**
-	 * 
-	 * 定义Pointcut，Pointcut的名称为addAddMethod()，此方法没有返回值和参数
-	 * 
-	 * 该方法就是一个标识，不进行调用
-	 */
-	private void addAddMethod() {
-		System.out.println("-------addAddMethod-------");
-	};
-
-	private void delAddMethod3() {
-		System.out.println("-------delAddMethod-------");
-	};
-
 	/**
 	 * 
 	 * 定义Advice，表示我们的Advice应用到哪些Pointcut订阅的Joinpoint上
 	 */
+	@SuppressWarnings("unused")
 	private void checkSecurity() {
 		System.out.println("-------checkSecurity-------");
 	}
@@ -28,6 +14,7 @@ public class AopHandler {
 	 * 
 	 * 定义Advice，表示我们的Advice应用到哪些Pointcut订阅的Joinpoint上
 	 */
+	@SuppressWarnings("unused")
 	private void logSecurity() {
 		System.out.println("-------logSecurity-------");
 	}
