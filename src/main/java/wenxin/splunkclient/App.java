d9a80d49d534af8936c112608f1f9e1e197bd5ad
Billy Dai <2640736909@qq.com>
2015-03-10 11:23:58 +0800

    init splunk client in java

---
+++ src/main/java/wenxin/splunkclient/App.java
@@ -0,0 +1,13 @@
+package wenxin.splunkclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
