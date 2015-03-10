a0f32421b9712911310a996e42e10548485c8e7e
Billy Dai <2640736909@qq.com>
2015-03-10 11:58:48 +0800

    add test cases. testSearch fail

--- src/main/java/wenxin/splunkclient/App.java
+++ src/main/java/wenxin/splunkclient/App.java
@@ -1,13 +1,60 @@
 package wenxin.splunkclient;
 
+import com.splunk.Service;
+import com.splunk.ServiceArgs;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	Service service;
+	private String pwd;
+	private String user;
+	private int port;
+	private String server;
+
+	public void bind(String server, int port, String user, String pwd) {
+		this.server = server;
+		this.port = port;
+		this.user = user;
+		this.pwd = pwd;
+
+	}
+
+	public String search(String search) {
+		return "{}";
+	}
+
+	public String config(String config, String value) {
+		return value;
+	}
+
+	public void log(String log) {
+	}
+
+	public void log(Byte[] log) {
+	}
+
+	public boolean connect() {
+		// Create a map of arguments and add login parameters
+		ServiceArgs loginArgs = new ServiceArgs();
+		loginArgs = new ServiceArgs();
+		loginArgs.setUsername(user);
+		loginArgs.setPassword(pwd);
+		loginArgs.setHost(server);
+		loginArgs.setPort(port);
+		// Create a Service instance and log in with the argument map
+		service = Service.connect(loginArgs);
+		return service != null;
+	}
+
+	public void disconnect() {
+		// disconnect
+		service.logout();
+	}
 }
--- src/test/java/wenxin/splunkclient/AppTest.java
+++ src/test/java/wenxin/splunkclient/AppTest.java
@@ -7,32 +7,46 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	App app;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public AppTest(String testName) {
+		super(testName);
+		app = new App();
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testBind() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(true);
+	}
+
+	public void testSearch() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		app.connect();
+		app.search("");
+		app.disconnect();
+		assertTrue(true);
+	}
+
+	public void testConfigureation() {
+		app.config("", "");
+		assertTrue(true);
+	}
+
+	public void testLogText() {
+		app.log("");
+		assertTrue(true);
+	}
+
+	public void testLogBinary() {
+		app.log(new Byte[1]);
+		assertTrue(true);
+	}
 }
