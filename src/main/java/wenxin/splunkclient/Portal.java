f2f0fe561ddfad8fa71545c3cd7c0ac9c37a69e6
Billy Dai <2640736909@qq.com>
2015-03-11 16:38:21 +0800

    event type cases

---
+++ src/main/java/wenxin/splunkclient/Portal.java
@@ -0,0 +1,229 @@
+package wenxin.splunkclient;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import com.splunk.Application;
+import com.splunk.Args;
+import com.splunk.CollectionArgs;
+import com.splunk.Command;
+import com.splunk.ConfCollection;
+import com.splunk.Entity;
+import com.splunk.EntityCollection;
+import com.splunk.EventType;
+import com.splunk.EventTypeCollection;
+import com.splunk.HttpException;
+import com.splunk.Service;
+import com.splunk.ServiceArgs;
+import com.splunk.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class Portal {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	Service service;
+	private String pwd;
+	private String user;
+	private int port;
+	private String server;
+	Integer connection_count;
+
+	public void bind(String server, int port, String user, String pwd) {
+		this.server = server;
+		this.port = port;
+		this.user = user;
+		this.pwd = pwd;
+
+	}
+
+	public String search(String search) {
+		return "{}";
+	}
+
+	public String config(String config, String value) {
+		return value;
+	}
+
+	public void log(String log) {
+	}
+
+	public void log(Byte[] log) {
+	}
+
+	public boolean connect() {
+		// Create a map of arguments and add login parameters
+		ServiceArgs loginArgs = new ServiceArgs();
+		loginArgs = new ServiceArgs();
+		loginArgs.setUsername(user);
+		loginArgs.setPassword(pwd);
+		loginArgs.setHost(server);
+		loginArgs.setPort(port);
+		// Create a Service instance and log in with the argument map
+		try {
+			service = Service.connect(loginArgs);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return service != null;
+	}
+
+	public void disconnect() {
+		// disconnect
+		service.logout();
+	}
+
+	public void users() {
+		for (User user : service.getUsers().values())
+			System.out.println(user.getName() + " " + user.getPassword() + " "
+					+ user.getDefaultApp());
+	}
+
+	public void configs() {
+		int count = 30;
+		int offset = 0;
+		ConfCollection confs;
+		CollectionArgs collargs = new CollectionArgs();
+		collargs.setCount(count);
+		collargs.setOffset(offset);
+
+		confs = service.getConfs(collargs);
+		// Got the first 30 elements
+		for (EntityCollection<Entity> conf : confs.values()) {
+			System.out.println(conf.getName());
+			// for (Entry<String, Entity> entity : conf.entrySet()) {
+			// System.out.println(entity.getKey() + " "
+			// + entity.getValue().toString());
+			// }
+		}
+		System.out.println();
+
+		// offset = offset + 30;
+		// collargs.setOffset(offset);
+		// confs = service.getConfs(collargs);
+		// // Got the next 30 elements
+		// for (EntityCollection<Entity> conf : confs.values()) {
+		// System.out.println(conf.getName());
+		// // for (Entry<String, Entity> entity : conf.entrySet()) {
+		// // System.out.println(entity.getKey() + " "
+		// // + entity.getValue().toString());
+		// // }
+		// }
+		// System.out.println();
+		//
+		// offset = offset + 30;
+		// collargs.setOffset(offset);
+		// confs = service.getConfs(collargs);
+		// // Got the next 30 elements
+		// for (EntityCollection<Entity> conf : confs.values()) {
+		// System.out.println(conf.getName());
+		// // for (Entry<String, Entity> entity : conf.entrySet()) {
+		// // System.out.println(entity.getKey() + " "
+		// // + entity.getValue().toString());
+		// // }
+		// }
+		// System.out.println();
+	}
+
+	void command(String[] args) throws IOException {
+		Command command = Command.splunk("search");
+		command.parse(args);
+		if (command.args.length != 1)
+			Command.error("Search expression required");
+		String query = command.args[0];
+
+		Service service = Service.connect(command.opts);
+
+		// Check the syntax of the query.
+		try {
+			Args parseArgs = new Args("parse_only", true);
+			service.parse(query, parseArgs);
+		} catch (HttpException e) {
+			String detail = e.getDetail();
+			Command.error("query '%s' is invalid: %s", query, detail);
+		}
+
+		// This is the simplest form of searching splunk. Note that additional
+		// arguments are allowed, but they are not shown in this example.
+		InputStream stream = service.oneshotSearch(query);
+
+		InputStreamReader reader = new InputStreamReader(stream, "UTF8");
+		try {
+			OutputStreamWriter writer = new OutputStreamWriter(System.out);
+			try {
+				int size = 1024;
+				char[] buffer = new char[size];
+				while (true) {
+					int count = reader.read(buffer);
+					if (count == -1)
+						break;
+					writer.write(buffer, 0, count);
+				}
+
+				writer.write("\n");
+			} finally {
+				writer.close();
+			}
+		} finally {
+			reader.close();
+		}
+	}
+
+	public String createApp(String name, String folder, boolean visible,
+			String description, String template) {
+		Application app;
+		EntityCollection<Application> apps = service.getApplications();
+		return name;
+	}
+
+	public void uploadAsset(String a2, File file) {
+	}
+
+	public String createEventType(String name, String filter,
+			String description, int priority) {
+		EventTypeCollection eventTypeCollection = service.getEventTypes();
+		EventType eventType = eventTypeCollection.create(name, filter);
+		eventType.setDescription(description);
+		eventType.setPriority(priority);
+		eventType.update();
+		return name;
+	}
+
+	public void updateEventType(String name, String description) {
+		EventTypeCollection eventTypeCollection = service.getEventTypes();
+		EventType eventType = eventTypeCollection.get(name);
+		eventType.setDescription(description);
+		eventType.update();
+	}
+
+	public void updateEventType(String name, int priority) {
+		EventTypeCollection eventTypeCollection = service.getEventTypes();
+		EventType eventType = eventTypeCollection.get(name);
+		eventType.setPriority(priority);
+		eventType.update();
+
+	}
+
+	public void updateEventType(String name, String description, int priority) {
+		EventTypeCollection eventTypeCollection = service.getEventTypes();
+		EventType eventType = eventTypeCollection.get(name);
+		eventType.setDescription(description);
+		eventType.setPriority(priority);
+		eventType.update();
+	}
+
+	public String destroyEventType(String name) {
+		EventTypeCollection eventTypeCollection = service.getEventTypes();
+		eventTypeCollection.remove(name);
+		eventTypeCollection.refresh();
+		return name;
+	}
+}
--- src/test/java/wenxin/splunkclient/AppTest.java
+++ src/test/java/wenxin/splunkclient/AppTest.java
@@ -1,5 +1,8 @@
 package wenxin.splunkclient;
 
+import java.io.File;
+import java.io.IOException;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -8,11 +11,11 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest extends TestCase {
-	App app;
+	Portal app;
 
 	public AppTest(String testName) {
 		super(testName);
-		app = new App();
+		app = new Portal();
 	}
 
 	/**
@@ -35,6 +38,22 @@ public class AppTest extends TestCase {
 		assertTrue(true);
 	}
 
+	public void testUser() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		app.users();
+		app.disconnect();
+		assertTrue(true);
+	}
+
+	public void testConfig() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		app.configs();
+		app.disconnect();
+		assertTrue(true);
+	}
+
 	public void testConfigureation() {
 		app.config("", "");
 		assertTrue(true);
@@ -47,6 +66,57 @@ public class AppTest extends TestCase {
 
 	public void testLogBinary() {
 		app.log(new Byte[1]);
+		assertTrue(true);
+	}
+
+	public void testCommand() {
+		try {
+			app.command(new String[] { "search * |  head 10",
+					"--host=localhost", "--port=8089", "--scheme=https",
+					"--username=admin", "--password=a123456" });
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertTrue(true);
+	}
+
+	public void testCreateEventType() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		String a1 = app.createEventType("test event", "index=_internal *",
+				"this is a test", 3);
+		app.disconnect();
+		assertTrue(true);
+	}
+
+	public void testModifyEventType() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		app.updateEventType("test event", "this is a new test");
+		app.updateEventType("test event", 4);
+		app.disconnect();
+		assertTrue(true);
+	}
+
+	public void testDestroyEventType() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		String a1 = app.destroyEventType("test event");
+		app.disconnect();
+		assertTrue(true);
+	}
+
+	public void testCreateApp() {
+		app.bind("localhost", 8089, "admin", "a123456");
+		assertTrue(app.connect());
+		String a1 = app.createApp("Billy test App 1", "billy_t1", false,
+				"this is a test app", "barebones");
+		String a2 = app.createApp("Billy test App 2", "billy_t2", true,
+				"this is a test app", "sample_app");
+		app.uploadAsset(a1, new File(""));
+		app.uploadAsset(a2, new File(""));
+		app.disconnect();
 		assertTrue(true);
 	}
 }
